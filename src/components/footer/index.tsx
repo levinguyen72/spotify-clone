9725231f492203186f6b368515ece5d5b6c9dffc
longnguyenthanh5898 <louis.nguyen8022@gmail.com>
2022-11-08 20:57:27 +0700

    update choose song

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -44,6 +44,7 @@ const Footer: React.FunctionComponent<IFooter> = ({}) => {
   };
   const setPlay = () => {
     startResumePlayback();
+
     setIsPlaying(true);
   };
   const setRepeat = () => {
--- src/services/startResumePlayback.ts
+++ src/services/startResumePlayback.ts
@@ -1,13 +1,11 @@
 import { getDeviceID } from './getIdDevices';
 import axios  from 'axios';
-//import scopes from '../configs/scopes'
 
-// const [currentPlaying, setCurrentPlaying] = useState()
 import { getToken } from './authService';
 import { _fakeToken } from '../configs';
 
 const START_RESUME_PLAYBACK_ENDPOINT = `https://api.spotify.com/v1/me/player/play`
-// const device_id =  `0d1841b0976bae2a3a310dd74c0f3df354899bc8`
+
 
 const device_id = getDeviceID
 
@@ -17,27 +15,24 @@ export const startResumePlayback = async () => {
     
     const _token = getToken().token
 
-    
-    // const { data } =
-    // try {
         const plMusic = await axios.put(
             START_RESUME_PLAYBACK_ENDPOINT,
             device_id,
-            // `https://api.spotify.com/v1/me/player/play?device_id=${DEVICE_ID}`,
+           
             {
-                
            
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
-                    //Authorization: `Bearer ${_token}`,
+                   
                     Authorization: `Bearer ${_fakeToken}`
-                    // Authorization: `Bear BQAn_Aw1bHR6_bfc2BTuTKsf_0RWiPp5UiGwqsdcaeLSGUop_9AoUGdRPxoV-zoTE8WMVRsQYH-e55YT7SI3H3rhX3DTQJw-cq0g7oiuZYBsboa3dsxQ-2FyLvWAlnN-qkcrXYv-0tfA817XjjCHFDVnBfUyGOK15ge0UiGXlzPfIoFXX5ecS6D5M2j8ouN9MOsIgxwf5LA`
+                   
                 },
                 params: {
-                    // device_id: "0d1841b0976bae2a3a310dd74c0f3df354899bc8",
+                   
                     
-                        "context_uri":  "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
+                     "context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
+                    // "context_uri":  `spotify:album:${id}`,
                         "offset": {
                         "position": 5
                         },
